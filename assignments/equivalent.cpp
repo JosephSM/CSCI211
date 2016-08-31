ddad55bfb2601680b0c3bdef07247ed6d070792d
Joseph Schwartz-Messing <josephsmy94@gmail.com>
2016-08-31 17:02:18 -0400

    finished equivalent.cpp

--- assignments/equivalent.cpp
+++ assignments/equivalent.cpp
@@ -1,19 +1,7 @@
 #include <iostream>
 using namespace std;
 
-
-int findStart(int arr1[], int len, int arr2[]){
-	for(int i = 0; i < len; i++){
-		if(arr1[0] == arr2[i]) return i;
-	}
-	return -1;
-}
-
-bool isEquiv(int arr1[], int len, int arr2[]){
-	int start = findStart(arr1, len, arr2);
-	if(start == -1) return false;
-
-
+bool isEquiv(int arr1[], int len, int arr2[], int start){
  	for(int i = 0; i < len; i++){
 		cout << arr1[i] << " = " << arr2[start%len] << endl;
 		if(arr1[i] != arr2[start%len]){
@@ -25,12 +13,16 @@ bool isEquiv(int arr1[], int len, int arr2[]){
 }
 int main(){
 	int arrlen = 5;
-	int arr1[5] = {1,2,3,4,5};
-	int arr2[5] = {3,4,5,1,2};
+	int arr1[5] = {1,2,3,1,5};
+	int arr2[5] = {3,1,5,1,2};
 
-
-	if(isEquiv(arr1, arrlen, arr2)) cout << "Equivalent!";
-	else cout << "Not Equivalent!!" << endl;
+  for(int i = 0; i < arrlen; i++)
+		if(arr1[0] == arr2[i]){
+			if(isEquiv(arr1, arrlen, arr2, i))
+				cout << "Equivalent!";	
+			else
+				cout << "Not Equivalent!!" << endl;
+		}
 	return 0;
 }
 
