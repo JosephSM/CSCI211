d774aa6585c571f033c3897fb915f455b4c5473f
Joseph Schwartz-Messing <josephsmy94@gmail.com>
2016-09-02 18:12:03 -0400

    added more stuff

---
+++ assignments/8qns.py
@@ -0,0 +1,5 @@
+from pprint import pprint
+board =[[0 for y in range(8) ] for x in range(8)]
+
+for r,c in board:
+	print(r, c) 
--- assignments/perfectsquare.cpp
+++ assignments/perfectsquare.cpp
@@ -9,15 +9,19 @@ bool last_two_digs_are_odd(long n){
 
 int main(){
 	long i = 4;
-	while(true){
+	while(i < 100){
+		cout << i << "^2 = " << i*i << endl;
 		if(last_two_digs_are_odd(i*i)){
-			cout << i << "^2 = " << i*i << endl;
+			cout << i*i << "we hit ODD!!" << endl;
 			break;		
 		}
 		i++;
 	}
-	cout << "Finished";
-	return 0;
+	cout << "Finished!, No number exists because\n";
+	cout << "by 100 the last two digs will repeat\n";
+	cout << "simple example: 236 * 236 = 55696 \n"<<
+					"                136 * 136 = 18496 \n"<<
+					"                 36 * 36  =  1296";
 }
 
 
